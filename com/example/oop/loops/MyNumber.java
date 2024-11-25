281cf257613ed2e32eddbe7d799dde1f89d2a167
waseem2272 <waseem2k181@gmail.com>
2024-11-26 01:02:02 +0500

    find out prime numbers

---
+++ com/example/oop/loops/MyNumber.java
@@ -0,0 +1,18 @@
+package com.example.oop.loops;
+
+public class MyNumber {
+
+    private int number;
+    public MyNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isPrime() {
+
+        if (this.number % 2 == 0) {
+
+        }
+
+        return false;
+    }
+}
---
+++ com/example/oop/loops/MyNumberRunner.java
@@ -0,0 +1,9 @@
+package com.example.oop.loops;
+
+public class MyNumberRunner {
+    public static void main(String[] args) {
+
+        MyNumber myNumber = new MyNumber(3);
+        System.out.println(myNumber.isPrime());
+    }
+}
