a2705b290ca7e8a8fc2d5366e71311f7d96efeda
waseem.ahmed <waseem.ahmed@simpaisa.com>
2024-11-25 18:57:02 +0500

    char practiced

--- com/example/oop/primitive_types_in_depth/MyChar.java
+++ com/example/oop/primitive_types_in_depth/MyChar.java
@@ -10,14 +10,49 @@ public class MyChar {
 
     public boolean isVowel() {
 
-        if (this.ch == 'a' || this.ch == 'A' || this.ch == 'e' || this.ch == 'E' || this.ch == 'i' || this.ch == 'I'
-        || this.ch == 'o' || this.ch == 'O' || this.ch == 'u' || this.ch == 'U')
+        if (this.ch == 'a' || this.ch == 'A' || this.ch == 'e' || this.ch == 'E' || this.ch == 'i'
+                || this.ch == 'I' || this.ch == 'o' || this.ch == 'O' || this.ch == 'u' || this.ch == 'U')
             return true;
 
         return false;
     }
 
     public boolean isDigit() {
+        // checks for 0-9 unicode
         return this.ch >= 48 && this.ch <= 57;
+    }
+
+    public boolean isAlphabet() {
+        // between 'a' to 'z'
+        if (this.ch >= 97 && this.ch <= 122)
+            return true;
+
+        // between 'A' to 'Z'
+        if (this.ch >= 65 && this.ch <= 90)
+            return true;
+
+        return false;
+    }
+
+    public boolean isConsonant() {
+        if (!this.isVowel())
+            return true;
+
+        return false;
+    }
+
+    public static void printLowerCaseAlphabets() {
+        // 'a' to 'z'
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            System.out.print(ch + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printUpperCaseAlphabets() {
+        // 'A' to 'Z'
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(ch + " ");
+        }
     }
 }
--- com/example/oop/primitive_types_in_depth/MyCharRunner.java
+++ com/example/oop/primitive_types_in_depth/MyCharRunner.java
@@ -1,11 +1,20 @@
 package com.example.oop.primitive_types_in_depth;
 
+import java.util.Set;
+
 public class MyCharRunner {
     public static void main(String[] args) {
 
-        MyChar myChar = new MyChar('9');
+        MyChar myChar = new MyChar('c');
         System.out.println(myChar.isVowel());
 
         System.out.println(myChar.isDigit());
+
+        System.out.println(myChar.isAlphabet());
+
+        System.out.println(myChar.isConsonant());
+
+        MyChar.printLowerCaseAlphabets();
+        MyChar.printUpperCaseAlphabets();
     }
 }
