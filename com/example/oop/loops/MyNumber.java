16dfeed6bd7d7382f3b4e506395772209e6e6a60
waseem2272 <waseem2k181@gmail.com>
2024-11-27 08:57:38 +0500

    find out prime numbers, sum upto n, no. of divisors and a number triangle

--- com/example/oop/loops/MyNumber.java
+++ com/example/oop/loops/MyNumber.java
@@ -3,23 +3,76 @@ package com.example.oop.loops;
 public class MyNumber {
 
     private int number;
+
+    public MyNumber() {}
     public MyNumber(int number) {
         this.number = number;
     }
 
-    public boolean isPrime() {
+//    public boolean isPrime() {
+//
+//        if (this.number <= 1) {
+//            return false;
+//        }
+//
+//        for (int i = 2; i <= this.number / 2; i++) {
+//            if (this.number % i == 0) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
 
+    // another way
+    public boolean isPrime() {
         if (this.number <= 1) {
             return false;
         }
-
-        for (int i = 2; i <= this.number / 2; i++) {
+        for (int i = 2; i <= Math.sqrt(this.number); i++) {
             if (this.number % i == 0) {
                 return false;
             }
         }
-
         return true;
     }
 
+    public int sumUptoN() {
+        // 1 + 2 + 3 + 4 ... n
+        int sum = 0;
+        for (int i = 1; i <= this.number; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int sumOfDivisors() {
+
+        // sum of the number except 1 and number itself
+        int sum = 0;
+
+        for (int i = 2; i <= this.number - 1; i++) {
+            if (this.number % i == 0) {
+                System.out.println("Divisors: " + i);
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public void printANumberTriangle() {
+
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
+
+        for (int i = 1; i <= this.number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- com/example/oop/loops/MyNumberRunner.java
+++ com/example/oop/loops/MyNumberRunner.java
@@ -5,25 +5,15 @@ import java.util.Scanner;
 public class MyNumberRunner {
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int n = s.nextInt();
-        if (isPrime(n)) {
-            System.out.println(n + " is a prime number");
-        } else {
-            System.out.println(n + " is not a prime number");
-        }
-    }
+        MyNumber myNumber = new MyNumber(6);
+        System.out.println(myNumber.isPrime());
 
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i < Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        int sum = myNumber.sumUptoN();
+        System.out.println("sumUptoN " + sum);
+
+        int sumOfDivisors = myNumber.sumOfDivisors();
+        System.out.println("sumOfDivisors " + sumOfDivisors);
+
+        myNumber.printANumberTriangle();
     }
 }
